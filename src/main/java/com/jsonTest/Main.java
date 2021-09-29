77fc32accb476d246ee1544d6e616a1fb1ba510c
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-09-29 11:00:04 +0200

    Convert Json text to PDFile Maven dependencies

--- src/main/java/com/jsonTest/Main.java
+++ src/main/java/com/jsonTest/Main.java
@@ -1,16 +1,19 @@
 package com.jsonTest;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -24,8 +27,10 @@ public class Main {
 		public static final Pattern VALID_EMAIL_ADDRESS_REGEX = 
 			    Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
+		@SuppressWarnings("deprecation")
 		public static void main(String[] args) throws IOException {
 
+			
 			
 			try {
 				
@@ -34,6 +39,7 @@ public class Main {
 				
 				if (filePath == null) {
 				throw new NullPointerException("Cannot find resource file " + filePath);
+				
 			}
 				
 
@@ -49,27 +55,6 @@ public class Main {
 				if(validate(email)) {
 					System.out.println("found "+email);
 				}
-					
-					
-					
-					// Take all Id in JSON File
-//					if (i <= 4) {
-//						String id = (String) jsonObject.get("_id");
-//						System.out.println(" ID:" + id);
-//					}
-//					
-//					
-//					Pattern p = Pattern.compile("[A-Z0-9._]+@");
-//					Matcher m = p.matcher();
-//					 
-//					 while(m.find()) {
-//						 
-//						 if (i <= 4) {
-//							 String email = jsonObject.getString("email");
-//							 System.out.println(m.group(email));
-//							 System.out.println(m.group(i));
-//						 }
-//					 }
 
 				}
 				
@@ -119,6 +104,33 @@ public class Main {
 					System.out.println(newFile);
 					
 					System.out.print(jsonObject);
+					
+
+					// Create PDF Document
+					PDDocument document = new PDDocument();
+					PDPage page = new PDPage();
+					
+					document.addPage(page);
+					
+					
+					PDFont font = PDType1Font.TIMES_BOLD_ITALIC;
+					
+					PDPageContentStream contentStream = new PDPageContentStream(document,page);
+					
+					// Text
+					contentStream.beginText();
+					contentStream.setFont(font, 15);
+					contentStream.showText("Adnann Muratovic " + " ----> " + email);
+					contentStream.setLeading(15);     
+				    contentStream.newLineAtOffset(175, 670);
+//					contentStream.newLineAtOffset(400, 200);
+					contentStream.endText();
+				
+					contentStream.close();
+					
+					document.save("NewPDFFile.pdf");
+					document.close();
+					
 					
 			
 			}
